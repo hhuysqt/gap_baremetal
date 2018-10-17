adf928493670d09c279a63f4197140f069a27ef4
hhuysqt <hyq9606@126.com>
2018-10-17 18:36:47 +0800

    build command

---
+++ build.sh
@@ -0,0 +1,10 @@
+riscv32-unknown-elf-gcc -o test \
+startup_GAP8.S \
+gap_fc_event.c gap_fll.c gap_hyperbus.c gap_pmu.c gap_uart.c gap_udma.c \
+main_UART.c mbed_pinmap_common.c PeripheralPins.c pinmap.c system_GAP8.c \
+-g -fno-jump-tables -fno-tree-loop-distribute-patterns \
+-Wextra -Wall -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wundef \
+-fdata-sections -ffunction-sections \
+-march=rv32imcxgap8 -mPE=8 -mFC=1 -D__riscv__ -D__pulp__ -D__GAP8__ \
+-MMD -MP -nostartfiles -O3 \
+-T GAP8.ld
