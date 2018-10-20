47974e2e84dc82959cf7b6ac99f13e289a62101a
hhuysqt <hyq9606@126.com>
2018-10-20 19:11:53 +0800

    add GPIO configurations and manipulations

--- gap_gpio.c
+++ gap_gpio.c
@@ -20,9 +20,9 @@
  * 
  * GPIO software abstraction: bitmap configuration of pins
  * 
- * |31 18|   17  |     16     |15              8|7      0|
- * | --- | drive | pull-up/OD | alt-func-number | pinnum |
- * | --- | 1-bit |   1-bit    |      2-bit      |  8-bit |
+ * |31 18|   17  |     16     |  15 |14   10|9     8|7      0|
+ * | --- | drive | pull-up/OD | I/O | GPIOn |  alt  | pinnum |
+ * | --- | 1-bit |   1-bit    | 1-b | 5-bit | 2-bit |  8-bit |
  *
  * Returned Value:
  *   OK on success
@@ -43,13 +43,41 @@ int gap8_configpin(uint32_t cfgset)
       return ERROR;
     }
 
-  /* Set pin drive strength (or pin speed in other words) and pulling */
+  /* Set pin drive strength (or pin speed in other words) and pulling
+   * If it's a GPIO, set input or output.
+   *
+   * Note that GPIO and non-GPIO uses different register sets...
+   * And all the GPIO functions are mapped to ALT-1, and ALT-1 contains
+   * only GPIO functions...
+   **/
 
   port_cfg_reg = PORTA->PADCFG[pinnum >> 2];
   shiftcnt = (pinnum & 0x3) << 3;
   port_cfg_reg &=     ~(0x3 << shiftcnt);
   port_cfg_reg |= pin_dr_pu << shiftcnt;
   PORTA->PADCFG[pinnum >> 2] = port_cfg_reg;
+
+  if (altfunc == 1)
+    {
+      uint32_t gpio_n = (cfgset >> 10) & 0x1f;
+      uint32_t gpio_dir = (cfgset >> 15) & 0x1;
+      uint32_t tmp;
+
+      /* It must be a GPIO */
+
+      GPIOA->EN |= (1L << gpio_n);
+
+      tmp = GPIOA->PADCFG[gpio_n >> 2];
+      shiftcnt = (gpio_n & 0x3) << 3;
+      tmp &=     ~(0x3 << shiftcnt);
+      tmp |= pin_dr_pu << shiftcnt;
+      GPIOA->PADCFG[gpio_n >> 2] = tmp;
+
+      tmp = GPIOA->DIR;
+      tmp &= ~(1L << gpio_n);
+      tmp |= gpio_dir << gpio_n;
+      GPIOA->DIR = tmp;
+    }
 
   /* Set pin alternative function */
 
@@ -67,12 +95,26 @@ int gap8_configpin(uint32_t cfgset)
  *
  * Description:
  *   Write one or zero to the selected GPIO pin
+ * 
+ * Bit encoded pinset:
+ * 
+ * |31 15|14   10|9   0|
+ * | --- | GPIOn | --- |
+ * | --- | 5-bit | --- |
  *
  ************************************************************************************/
 
 void gap8_gpiowrite(uint32_t pinset, bool value)
 {
-
+  uint32_t gpio_n = (pinset >> 10) & 0x1f;
+  if (value)
+    {
+      GPIOA->OUT |= (1L << gpio_n);
+    }
+  else
+    {
+      GPIOA->OUT &= ~(1L << gpio_n);
+    }
 }
 
 /************************************************************************************
@@ -81,11 +123,18 @@ void gap8_gpiowrite(uint32_t pinset, bool value)
  * Description:
  *   Read one or zero from the selected GPIO pin
  *
+ * Bit encoded pinset:
+ * 
+ * |31 15|14   10|9   0|
+ * | --- | GPIOn | --- |
+ * | --- | 5-bit | --- |
+ *
  ************************************************************************************/
 
 bool gap8_gpioread(uint32_t pinset)
 {
-
+  uint32_t gpio_n = (pinset >> 10) & 0x1f;
+  return (GPIOA->IN >> gpio_n) & 0x1;
 }
 
 /************************************************************************************
@@ -93,11 +142,30 @@ bool gap8_gpioread(uint32_t pinset)
  *
  * Description:
  *   Enable or disable interrupt on GPIO
+ * 
+ * Bit encoded pinset:
+ * 
+ * |31 20|19     18|17 15|14   10|9   0|
+ * | --- | int-typ | --- | GPIOn | --- |
+ * | --- |  2-bit  | --- | 5-bit | --- |
  *
  ************************************************************************************/
 
-bool gap8_gpioirqset(uint32_t pinset, bool enable)
+void gap8_gpioirqset(uint32_t pinset, bool enable)
 {
-  
+  uint32_t gpio_n = (pinset >> 10) & 0x1f;
+  uint32_t int_type = (pinset >> 18) * 0x3;
+  uint32_t tmp, shitfcnt;
+
+  if (enable)
+    {
+      shitfcnt = (gpio_n & 0xf) << 1;
+      tmp = GPIOA->INTCFG[gpio_n >> 4];
+      tmp &=    ~(0x3 << shitfcnt);
+      tmp |= int_type << shitfcnt;
+      GPIOA->INTCFG[gpio_n >> 4] = tmp;
+      
+      GPIOA->INTEN |= (1L << gpio_n);
+    }
 }
 
--- gap_gpio.h
+++ gap_gpio.h
@@ -27,141 +27,148 @@
 /*
  * GPIO software abstraction: bitmap configuration of pins
  * 
- * |31 18|   17  |     16     |15              8|7      0|
- * | --- | drive | pull-up/OD | alt-func-number | pinnum |
- * | --- | 1-bit |   1-bit    |      2-bit      |  8-bit |
+ * |31 20|19     18|   17  |     16     |  15 |14   10|9     8|7      0|
+ * | --- | int-typ | drive | pull-up/OD | I/O | GPIOn |  alt  | pinnum |
+ * | --- |  2-bit  | 1-bit |   1-bit    | 1-b | 5-bit | 2-bit |  8-bit |
  **/
+#define GAP8_GPIO_INT_FALLING             (0L << 18)
+#define GAP8_GPIO_INT_RISING              (1L << 18)
+#define GAP8_GPIO_INT_RISING_AND_FALLING  (2L << 18)
+
 #define GAP8_PIN_SPEED_HIGH   (1L << 17)
 #define GAP8_PIN_SPEED_LOW    (0L << 17)
 
 #define GAP8_PIN_PULL_UP      (1L << 16)
 #define GAP8_PIN_PULL_NONE    (0L << 16)
 
+#define GAP8_GPIO_INPUT       (0L << 15)
+#define GAP8_GPIO_OUTPUT      (1L << 15)
+
 #define GAP8_PIN_A4_SPIM1_MISO    ((0L << 8) | 0)
-#define GAP8_PIN_A4_GPIOA0        ((1L << 8) | 0)
+#define GAP8_PIN_A4_GPIOA0        ((1L << 8) | (0L << 10) | 0)
 
 #define GAP8_PIN_B3_SPIM1_MOSI    ((0L << 8) | 1)
-#define GAP8_PIN_B3_GPIOA1        ((1L << 8) | 1)
+#define GAP8_PIN_B3_GPIOA1        ((1L << 8) | (1L << 10) | 1)
 
 #define GAP8_PIN_A5_SPIM1_CS0     ((0L << 8) | 2)
-#define GAP8_PIN_A5_GPIOA2        ((1L << 8) | 2)
+#define GAP8_PIN_A5_GPIOA2        ((1L << 8) | (2L << 10) | 2)
 #define GAP8_PIN_A5_I2C1_SDA      ((2L << 8) | 2)
 
 #define GAP8_PIN_B4_SPIM1_SCK     ((0L << 8) | 3)
-#define GAP8_PIN_B4_GPIOA3        ((1L << 8) | 3)
+#define GAP8_PIN_B4_GPIOA3        ((1L << 8) | (3L << 10) | 3)
 #define GAP8_PIN_B4_I2C1_SCL      ((2L << 8) | 3)
 
 #define GAP8_PIN_A3_ORCA_TXSYNC   ((0L << 8) | 4)
-#define GAP8_PIN_A3_GPIOA0        ((1L << 8) | 4)
+#define GAP8_PIN_A3_GPIOA0        ((1L << 8) | (0L << 10) | 4)
 #define GAP8_PIN_A3_SPIM1_CS0     ((2L << 8) | 4)
 
 #define GAP8_PIN_B2_ORCA_RXSYNC   ((0L << 8) | 5)
-#define GAP8_PIN_B2_GPIOA1        ((1L << 8) | 5)
+#define GAP8_PIN_B2_GPIOA1        ((1L << 8) | (1L << 10) | 5)
 #define GAP8_PIN_B2_SPIM1_CS1     ((2L << 8) | 5)
 
 #define GAP8_PIN_A2_ORCA_TX1      ((0L << 8) | 6)
-#define GAP8_PIN_A2_GPIOA2        ((1L << 8) | 6)
+#define GAP8_PIN_A2_GPIOA2        ((1L << 8) | (2L << 10) | 6)
 
 #define GAP8_PIN_B1_ORCA_TXQ      ((0L << 8) | 7)
-#define GAP8_PIN_B1_GPIOA3        ((1L << 8) | 7)
+#define GAP8_PIN_B1_GPIOA3        ((1L << 8) | (3L << 10) | 7)
 
 #define GAP8_PIN_A44_ORCA_RXI     ((0L << 8) | 8)
-#define GAP8_PIN_A44_GPIOA4       ((1L << 8) | 8)
+#define GAP8_PIN_A44_GPIOA4       ((1L << 8) | (4L << 10) | 8)
 #define GAP8_PIN_A44_SPIS0_D0     ((2L << 8) | 8)
 #define GAP8_PIN_A44_SPIS0_D2     ((3L << 8) | 8)
 
 #define GAP8_PIN_B40_ORCA_RXQ     ((0L << 8) | 9)
-#define GAP8_PIN_B40_GPIOA5       ((1L << 8) | 9)
+#define GAP8_PIN_B40_GPIOA5       ((1L << 8) | (5L << 10) | 9)
 #define GAP8_PIN_B40_SPIS0_D1     ((2L << 8) | 9)
 #define GAP8_PIN_B40_SPIS0_D3     ((3L << 8) | 9)
 
 #define GAP8_PIN_A43_CAM_PCLK     ((0L << 8) | 10)
-#define GAP8_PIN_A43_GPIOA4       ((1L << 8) | 10)
+#define GAP8_PIN_A43_GPIOA4       ((1L << 8) | (4L << 10) | 10)
 #define GAP8_PIN_A43_TIM1_CH0     ((2L << 8) | 10)
 
 #define GAP8_PIN_A37_CAM_HSYNC    ((0L << 8) | 11)
-#define GAP8_PIN_A37_GPIOA5       ((1L << 8) | 11)
+#define GAP8_PIN_A37_GPIOA5       ((1L << 8) | (5L << 10) | 11)
 #define GAP8_PIN_A37_TIM1_CH1     ((2L << 8) | 11)
 
 #define GAP8_PIN_B39_CAM_D0       ((0L << 8) | 12)
-#define GAP8_PIN_B39_GPIOA6       ((1L << 8) | 12)
+#define GAP8_PIN_B39_GPIOA6       ((1L << 8) | (6L << 10) | 12)
 #define GAP8_PIN_B39_TIM1_CH2     ((2L << 8) | 12)
 
 #define GAP8_PIN_A42_CAM_D1       ((0L << 8) | 13)
-#define GAP8_PIN_A42_GPIOA7       ((1L << 8) | 13)
+#define GAP8_PIN_A42_GPIOA7       ((1L << 8) | (7L << 10) | 13)
 #define GAP8_PIN_A42_TIM1_CH3     ((2L << 8) | 13)
 
 #define GAP8_PIN_B38_CAM_D2       ((0L << 8) | 14)
-#define GAP8_PIN_B38_GPIOA8       ((1L << 8) | 14)
+#define GAP8_PIN_B38_GPIOA8       ((1L << 8) | (8L << 10) | 14)
 #define GAP8_PIN_B38_TIM2_CH0     ((2L << 8) | 14)
 
 #define GAP8_PIN_A41_CAM_D3       ((0L << 8) | 15)
-#define GAP8_PIN_A41_GPIOA9       ((1L << 8) | 15)
+#define GAP8_PIN_A41_GPIOA9       ((1L << 8) | (9L << 10) | 15)
 #define GAP8_PIN_A41_TIM2_CH1     ((2L << 8) | 15)
 
 #define GAP8_PIN_B37_CAM_D4       ((0L << 8) | 16)
-#define GAP8_PIN_B37_GPIOA10      ((1L << 8) | 16)
+#define GAP8_PIN_B37_GPIOA10      ((1L << 8) | (10L << 10) | 16)
 #define GAP8_PIN_B37_TIM2_CH2     ((2L << 8) | 16)
 
 #define GAP8_PIN_A40_CAM_D5       ((0L << 8) | 17)
-#define GAP8_PIN_A40_GPIOA11      ((1L << 8) | 17)
+#define GAP8_PIN_A40_GPIOA11      ((1L << 8) | (11L << 10) | 17)
 #define GAP8_PIN_A40_TIM2_CH3     ((2L << 8) | 17)
 
 #define GAP8_PIN_B36_CAM_D6       ((0L << 8) | 18)
-#define GAP8_PIN_B36_GPIOA12      ((1L << 8) | 18)
+#define GAP8_PIN_B36_GPIOA12      ((1L << 8) | (12L << 10) | 18)
 #define GAP8_PIN_B36_TIM3_CH0     ((2L << 8) | 18)
 
 #define GAP8_PIN_A38_CAM_D7       ((0L << 8) | 19)
-#define GAP8_PIN_A38_GPIOA13      ((1L << 8) | 19)
+#define GAP8_PIN_A38_GPIOA13      ((1L << 8) | (13L << 10) | 19)
 #define GAP8_PIN_A38_TIM3_CH1     ((2L << 8) | 19)
 
 #define GAP8_PIN_A36_CAM_VSYNC    ((0L << 8) | 20)
-#define GAP8_PIN_A36_GPIOA14      ((1L << 8) | 20)
+#define GAP8_PIN_A36_GPIOA14      ((1L << 8) | (14L << 10) | 20)
 #define GAP8_PIN_A36_TIM3_CH2     ((2L << 8) | 20)
 
 #define GAP8_PIN_B34_I2C1_SDA     ((0L << 8) | 21)
-#define GAP8_PIN_B34_GPIOA15      ((1L << 8) | 21)
+#define GAP8_PIN_B34_GPIOA15      ((1L << 8) | (15L << 10) | 21)
 #define GAP8_PIN_B34_TIM3_CH3     ((2L << 8) | 21)
 
 #define GAP8_PIN_D1_I2C1_SCL      ((0L << 8) | 22)
-#define GAP8_PIN_D1_GPIOA16       ((1L << 8) | 22)
+#define GAP8_PIN_D1_GPIOA16       ((1L << 8) | (16L << 10) | 22)
 #define GAP8_PIN_D1_ORCA_CLK      ((2L << 8) | 22)
 
 #define GAP8_PIN_B11_TIM0_CH0     ((0L << 8) | 23)
-#define GAP8_PIN_B11_GPIOA17      ((1L << 8) | 23)
+#define GAP8_PIN_B11_GPIOA17      ((1L << 8) | (17L << 10) | 23)
 
 #define GAP8_PIN_A13_TIM0_CH1     ((0L << 8) | 24)
-#define GAP8_PIN_A13_GPIOA18      ((1L << 8) | 24)
+#define GAP8_PIN_A13_GPIOA18      ((1L << 8) | (18L << 10) | 24)
 #define GAP8_PIN_A13_TIM1_CH0     ((2L << 8) | 24)
 
 #define GAP8_PIN_B12_TIM0_CH2     ((0L << 8) | 25)
-#define GAP8_PIN_B12_GPIOA19      ((1L << 8) | 25)
+#define GAP8_PIN_B12_GPIOA19      ((1L << 8) | (19L << 10) | 25)
 #define GAP8_PIN_B12_TIM2_CH0     ((2L << 8) | 25)
 
 #define GAP8_PIN_A14_TIM0_CH3     ((0L << 8) | 26)
-#define GAP8_PIN_A14_GPIOA20      ((1L << 8) | 26)
+#define GAP8_PIN_A14_GPIOA20      ((1L << 8) | (20L << 10) | 26)
 #define GAP8_PIN_A14_TIM3_CH0     ((2L << 8) | 26)
 
 #define GAP8_PIN_B13_I2S1_SCK     ((0L << 8) | 27)
-#define GAP8_PIN_B13_GPIOA21      ((1L << 8) | 27)
+#define GAP8_PIN_B13_GPIOA21      ((1L << 8) | (21L << 10) | 27)
 #define GAP8_PIN_B13_SPIS0_SCK    ((2L << 8) | 27)
 #define GAP8_PIN_B13_I2S1_SDI     ((3L << 8) | 27)
 
 #define GAP8_PIN_A15_I2S1_WS      ((0L << 8) | 28)
-#define GAP8_PIN_A15_GPIOA22      ((1L << 8) | 28)
+#define GAP8_PIN_A15_GPIOA22      ((1L << 8) | (22L << 10) | 28)
 #define GAP8_PIN_A15_SPIS0_CS     ((2L << 8) | 28)
 #define GAP8_PIN_A15_HYPER_CKN    ((3L << 8) | 28)
 
 #define GAP8_PIN_B14_I2S1_SDI     ((0L << 8) | 29)
-#define GAP8_PIN_B14_GPIOA23      ((1L << 8) | 29)
+#define GAP8_PIN_B14_GPIOA23      ((1L << 8) | (23L << 10) | 29)
 #define GAP8_PIN_B14_SPIS0_D2     ((2L << 8) | 29)
 #define GAP8_PIN_B14_HYPER_CK     ((3L << 8) | 29)
 
 #define GAP8_PIN_B6_UART_RX       ((0L << 8) | 30)
-#define GAP8_PIN_B6_GPIOA24       ((1L << 8) | 30)
+#define GAP8_PIN_B6_GPIOA24       ((1L << 8) | (24L << 10) | 30)
 
 #define GAP8_PIN_A7_UART_TX       ((0L << 8) | 31)
-#define GAP8_PIN_A7_GPIOA25       ((1L << 8) | 31)
+#define GAP8_PIN_A7_GPIOA25       ((1L << 8) | (25L << 10) | 31)
 
 #define GAP8_PIN_D2_SPIM0_D0      ((0L << 8) | 32)
 #define GAP8_PIN_D2_HYPER_D0      ((3L << 8) | 32)
@@ -170,12 +177,12 @@
 #define GAP8_PIN_A11_HYPER_D1     ((3L << 8) | 33)
 
 #define GAP8_PIN_B10_SPIM0_D2     ((0L << 8) | 34)
-#define GAP8_PIN_B10_GPIOA26      ((1L << 8) | 34)
+#define GAP8_PIN_B10_GPIOA26      ((1L << 8) | (26L << 10) | 34)
 #define GAP8_PIN_B10_I2C1_SDA     ((2L << 8) | 34)
 #define GAP8_PIN_B10_HYPER_D2     ((3L << 8) | 34)
 
 #define GAP8_PIN_A10_SPIM0_D3     ((0L << 8) | 35)
-#define GAP8_PIN_A10_GPIOA27      ((1L << 8) | 35)
+#define GAP8_PIN_A10_GPIOA27      ((1L << 8) | (27L << 10) | 35)
 #define GAP8_PIN_A10_I2C1_SCL     ((2L << 8) | 35)
 #define GAP8_PIN_A10_HYPER_D3     ((3L << 8) | 35)
 
@@ -183,7 +190,7 @@
 #define GAP8_PIN_B8_HYPER_D4      ((3L << 8) | 36)
 
 #define GAP8_PIN_A8_SPIM0_CS1     ((0L << 8) | 37)
-#define GAP8_PIN_A8_GPIOA28       ((1L << 8) | 37)
+#define GAP8_PIN_A8_GPIOA28       ((1L << 8) | (28L << 10) | 37)
 #define GAP8_PIN_A8_SPIS0_D3      ((2L << 8) | 37)
 #define GAP8_PIN_A8_HYPER_D5      ((3L << 8) | 37)
 
@@ -191,17 +198,17 @@
 #define GAP8_PIN_B7_HYPER_D6      ((3L << 8) | 38)
 
 #define GAP8_PIN_A9_SPIS0_CS      ((0L << 8) | 39)
-#define GAP8_PIN_A9_GPIOA29       ((1L << 8) | 39)
+#define GAP8_PIN_A9_GPIOA29       ((1L << 8) | (29L << 10) | 39)
 #define GAP8_PIN_A9_SPIM1_CS0     ((2L << 8) | 39)
 #define GAP8_PIN_A9_HYPER_D7      ((3L << 8) | 39)
 
 #define GAP8_PIN_B15_SPIS0_D0     ((0L << 8) | 40)
-#define GAP8_PIN_B15_GPIOA30      ((1L << 8) | 40)
+#define GAP8_PIN_B15_GPIOA30      ((1L << 8) | (30L << 10) | 40)
 #define GAP8_PIN_B15_SPIM1_CS1    ((2L << 8) | 40)
 #define GAP8_PIN_B15_HYPER_CS0    ((3L << 8) | 40)
 
 #define GAP8_PIN_A16_SPIS0_D1     ((0L << 8) | 41)
-#define GAP8_PIN_A16_GPIOA31      ((1L << 8) | 41)
+#define GAP8_PIN_A16_GPIOA31      ((1L << 8) | (31L << 10) | 41)
 #define GAP8_PIN_A16_HYPER_CS1    ((3L << 8) | 41)
 
 #define GAP8_PIN_B9_SPIS0_SCK     ((0L << 8) | 42)
@@ -260,7 +267,7 @@ bool gap8_gpioread(uint32_t pinset);
  *
  ************************************************************************************/
 
-bool gap8_gpioirqset(uint32_t pinset, bool enable);
+void gap8_gpioirqset(uint32_t pinset, bool enable);
 
 
 #endif
